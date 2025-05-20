9892a6fb75622f8fe76017ed76716c9be72b1dc1
Jayeshkulkarni17 <jaykulkarni17ab@gmail.com>
2025-05-20 21:14:42 +0530

    initial commit next js and node js setup

---
+++ client/pages/index.tsx
@@ -0,0 +1,17 @@
+import React, { useEffect, useState } from "react";
+
+function index() {
+  const [message, setMessage] = useState("loading");
+  useEffect(() => {
+    fetch("http://localhost:8080/api/home")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setMessage(data.message);
+      });
+  }, []);
+
+  return <div>{message}</div>;
+}
+
+export default index;
